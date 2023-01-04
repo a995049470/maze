dd6d196b4861f354c6f21d42b6c6aa525a7b597c
a995049470 <17855834269@163.com>
2023-01-04 22:59:05 +0800

    1

---
+++ Maze/Code/Render/LSprite3DBatch.cs
@@ -0,0 +1,22 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Graphics;
+using Stride.Rendering;
+
+namespace Maze.Code.Render
+{
+    public class LSprite3DBatch : Sprite3DBatch
+    {
+        protected ValueParameter<float> cellValue;
+        private EffectInstance defaultSpriteEffect;
+
+        public LSprite3DBatch(GraphicsDevice device, int bufferElementCount = 1024, int batchCapacity = 64) : base(device, bufferElementCount, batchCapacity)
+        {
+        }
+
+        protected override void PrepareParameters()
+        {
+            base.PrepareParameters();
+        }
+
+    }
+}
