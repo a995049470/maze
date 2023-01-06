2533a527e2bdf31efbda1d51701484aa368ffc7e
无妄妖 <995049470@qq.com>
2023-01-06 19:31:25 +0800

    1.修改

--- Maze/Code/Render/LSprite3DBatch.cs
+++ Maze/Code/Render/LSprite3DBatch.cs
@@ -6,17 +6,36 @@ namespace Maze.Code.Render
 {
     public class LSprite3DBatch : Sprite3DBatch
     {
-        protected ValueParameter<float> cellValue;
+        protected class DrawParameter
+        {
+            public Texture Texture;
+            public float CellValue;
+        }
+
+
+        public static readonly ValueParameterKey<float> CellValue = ParameterKeys.NewValue<float>();
+
+        protected ValueParameter<float>? cellValue;
         private EffectInstance defaultSpriteEffect;
 
         public LSprite3DBatch(GraphicsDevice device, int bufferElementCount = 1024, int batchCapacity = 64) : base(device, bufferElementCount, batchCapacity)
         {
+
+        }
+
+        protected void Draw(DrawParameter parameter, in ElementInfo elementInfo)
+        {
+
         }
 
         protected override void PrepareParameters()
         {
             base.PrepareParameters();
+            cellValue = null;
+            if (Effect.Effect.HasParameter(CellValue))
+                cellValue = Effect.Parameters.GetAccessor(CellValue);
         }
 
+        
     }
 }
