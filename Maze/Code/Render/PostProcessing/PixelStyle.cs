e4b85fd18bb15cc6e18b616cbf149295b05500af
无妄妖 <995049470@qq.com>
2023-03-17 23:10:23 +0800

    ,,,,

---
+++ Maze/Code/Render/PostProcessing/PixelStyle.cs
@@ -0,0 +1,25 @@
+﻿using Stride.Core;
+using Stride.Graphics;
+using Stride.Rendering.Rendering.Images;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze.Code.Render
+{
+    [DataContract]
+    public class PixelStyle : BasePostProcessingEffect
+    {
+        public PixelStyle() : base("PixelStyleEffect")
+        {
+
+        }
+        public override bool IsVaild(Texture[] inputs, Texture output)
+        {
+            bool isVaild = inputs?.Length > 0 && inputs[0] != null && output != null;
+            return isVaild;
+        }
+    }
+}
