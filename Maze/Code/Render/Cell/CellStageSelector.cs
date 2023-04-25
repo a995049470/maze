20cea8abcba93058452009e9dada8cd97c29426f
a995049470 <17855834269@163.com>
2023-04-25 09:44:08 +0800

    '....'

--- Maze/Code/Render/Cell/CellStageSelector.cs
+++ Maze/Code/Render/Cell/CellStageSelector.cs
@@ -1,13 +1,32 @@
 ﻿using Stride.Rendering;
 using System;
+using System.ComponentModel;
 
 namespace Maze.Code.Render
 {
     public class CellStageSelector : RenderStageSelector
     {
+        [DefaultValue(RenderGroupMask.All)]
+        public RenderGroupMask RenderGroup { get; set; } = RenderGroupMask.All;
+        [DefaultValue(null)]
+        public RenderStage TransmittanceStage;
+        [DefaultValue(null)]
+        public RenderStage VisionStage;
+        public string EffectName;
+
         public override void Process(RenderObject renderObject)
         {
-            throw new NotImplementedException();
+            if (((RenderGroupMask)(1U << (int)renderObject.RenderGroup) & RenderGroup) != 0 && renderObject is CellRenderMesh cellRenderMesh)
+            {
+                if(TransmittanceStage != null && cellRenderMesh.Flag == CellFlag.Barrier)
+                {
+                    renderObject.ActiveRenderStages[TransmittanceStage.Index] = new ActiveRenderStage(EffectName);
+                }
+                else if(VisionStage != null && cellRenderMesh.Flag == CellFlag.Light)
+                {
+                    renderObject.ActiveRenderStages[VisionStage.Index] = new ActiveRenderStage(EffectName);
+                }
+            }
         }
     }
 }
